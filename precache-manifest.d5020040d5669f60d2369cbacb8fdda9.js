cc3d37028ab27f11b8ea60438349d9aa2c245d35
Teerapong Manakul <teerapong.m@zanroo.com>
2018-12-04 18:39:43 +0700

    Updates

---
+++ precache-manifest.d5020040d5669f60d2369cbacb8fdda9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chords/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "aaabaf138fa7d5beb021",
+    "url": "/chords/static/js/runtime~main.aaabaf13.js"
+  },
+  {
+    "revision": "c4983530fee6e0e114d5",
+    "url": "/chords/static/js/main.c4983530.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/chords/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "c4983530fee6e0e114d5",
+    "url": "/chords/static/css/main.fd48a93b.chunk.css"
+  },
+  {
+    "revision": "f7cffec7d853161a5655d66be16a0508",
+    "url": "/chords/index.html"
+  }
+];
